a2248dfa028d1bfa940a3afccd52c71ea5bcbe11
deanosborne <45112209+deanosborne@users.noreply.github.com>
2020-04-17 00:06:46 +1200

    Game revamp
    - Using low poly kit as base
    - Player controller has been changed to make the 'player' (lp_guy) first person
    - low poly kit will provide base for quests
    - will also provide base for inventory, pick ups etc
    - scenes will be made with unmade textures then will be fixed once game logic done
    - most assets are needed in this, however some may linger which wont be needed, will fix in future pushes

---
+++ Game/Meihana/Assets/Standard Assets/Scripts/PlayerController.cs
@@ -0,0 +1,610 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using Random = UnityEngine.Random;
+using UnityStandardAssets.Characters.FirstPerson;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(AudioSource))]
+public class PlayerController : MonoBehaviour
+{
+    #region Private Members
+
+    private Animator _animator;
+
+    private CharacterController m_characterController;
+
+
+    private InventoryItemBase mCurrentItem = null;
+
+    private HealthBar mHealthBar;
+
+    private HealthBar mFoodBar;
+
+    private int startHealth;
+
+    private int startFood;
+    private bool mCanTakeDamage = true;
+    [SerializeField] private MouseLook m_MouseLook;
+    private Camera m_Camera;
+    [SerializeField] private bool m_UseFovKick;
+    [SerializeField] private FOVKick m_FovKick = new FOVKick();
+    [SerializeField] private bool m_UseHeadBob;
+    [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
+    [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
+    private Vector3 m_OriginalCameraPosition;
+    [SerializeField] private bool m_IsWalking;
+    [SerializeField] private float m_WalkSpeed;
+    [SerializeField] private float m_RunSpeed;
+
+    [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
+    [SerializeField] private float m_JumpSpeed;
+    [SerializeField] private float m_StickToGroundForce;
+    [SerializeField] private float m_GravityMultiplier;
+    [SerializeField] private float m_StepInterval;
+    [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
+    [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
+    [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+
+
+    #endregion
+
+    #region Public Members
+
+    public Inventory Inventory;
+
+    public GameObject Hand;
+
+    public HUD Hud;
+
+    public event EventHandler PlayerDied;
+
+
+    #endregion
+
+    public UnityEvent QuestCompleted;
+    private bool m_Jump;
+    private float m_YRotation;
+    private Vector2 m_Input;
+    private Vector3 m_MoveDir = Vector3.zero;
+    [SerializeField] private CharacterController m_CharacterController;
+    private CollisionFlags m_CollisionFlags;
+    private bool m_PreviouslyGrounded;
+    private float m_StepCycle;
+    private float m_NextStep;
+    private bool m_Jumping;
+    private AudioSource m_AudioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        m_characterController = GetComponent<CharacterController>();
+        Inventory.ItemUsed += Inventory_ItemUsed;
+        Inventory.ItemRemoved += Inventory_ItemRemoved;
+        m_Camera = Camera.main;
+        m_MouseLook.Init(transform, m_Camera.transform);
+
+        mHealthBar = Hud.transform.Find("Bars_Panel/HealthBar").GetComponent<HealthBar>();
+        mHealthBar.Min = 0;
+        mHealthBar.Max = Health;
+        startHealth = Health;
+        mHealthBar.SetValue(Health);
+
+        mFoodBar = Hud.transform.Find("Bars_Panel/FoodBar").GetComponent<HealthBar>();
+        mFoodBar.Min = 0;
+        mFoodBar.Max = Food;
+        startFood = Food;
+        mFoodBar.SetValue(Food);
+
+        InvokeRepeating("IncreaseHunger", 0, HungerRate);
+    }
+
+
+    #region Inventory
+
+    private void Inventory_ItemRemoved(object sender, InventoryEventArgs e)
+    {
+        InventoryItemBase item = e.Item;
+
+        GameObject goItem = (item as MonoBehaviour).gameObject;
+        goItem.SetActive(true);
+        goItem.transform.parent = null;
+
+        if (item == mCurrentItem)
+            mCurrentItem = null;
+
+    }
+
+    private void SetItemActive(InventoryItemBase item, bool active)
+    {
+        GameObject currentItem = (item as MonoBehaviour).gameObject;
+        currentItem.SetActive(active);
+        currentItem.transform.parent = active ? Hand.transform : null;
+    }
+
+
+    private void Inventory_ItemUsed(object sender, InventoryEventArgs e)
+    {
+        if (e.Item.ItemType != EItemType.Consumable)
+        {
+            // If the player carries an item, un-use it (remove from player's hand)
+            if (mCurrentItem != null)
+            {
+                SetItemActive(mCurrentItem, false);
+            }
+
+            InventoryItemBase item = e.Item;
+
+            // Use item (put it to hand of the player)
+            SetItemActive(item, true);
+
+            mCurrentItem = e.Item;
+        }
+
+    }
+
+    private int Attack_1_Hash = Animator.StringToHash("Base Layer.Attack_1");
+
+    public bool IsAttacking
+    {
+        get
+        {
+            AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+            if (stateInfo.fullPathHash == Attack_1_Hash)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public void DropCurrentItem()
+    {
+        mCanTakeDamage = false;
+
+        _animator.SetTrigger("tr_drop");
+
+        GameObject goItem = (mCurrentItem as MonoBehaviour).gameObject;
+
+        Inventory.RemoveItem(mCurrentItem);
+
+        // Throw animation
+        Rigidbody rbItem = goItem.AddComponent<Rigidbody>();
+        if (rbItem != null)
+        {
+            rbItem.AddForce(transform.forward * 2.0f, ForceMode.Impulse);
+
+            Invoke("DoDropItem", 0.25f);
+        }
+    }
+
+    public void DropAndDestroyCurrentItem()
+    {
+        GameObject goItem = (mCurrentItem as MonoBehaviour).gameObject;
+
+        Inventory.RemoveItem(mCurrentItem);
+
+        Destroy(goItem);
+
+        mCurrentItem = null;
+    }
+
+    public void DoDropItem()
+    {
+        mCanTakeDamage = true;
+        if (mCurrentItem != null)
+        {
+            // Remove Rigidbody
+            Destroy((mCurrentItem as MonoBehaviour).GetComponent<Rigidbody>());
+
+            mCurrentItem = null;
+
+            mCanTakeDamage = true;
+        }
+    }
+
+    #endregion
+
+    #region Health & Hunger
+
+    [Tooltip("Amount of health")]
+    public int Health = 100;
+
+    [Tooltip("Amount of food")]
+    public int Food = 100;
+
+    [Tooltip("Rate in seconds in which the hunger increases")]
+    public float HungerRate = 0.5f;
+
+    public void IncreaseHunger()
+    {
+        Food--;
+        if (Food < 0)
+            Food = 0;
+
+        mFoodBar.SetValue(Food);
+
+        if (Food == 0)
+        {
+            CancelInvoke();
+            Die();
+        }
+    }
+
+    public bool IsDead
+    {
+        get
+        {
+            return Health == 0 || Food == 0;
+        }
+    }
+
+    public bool CarriesItem(string itemName)
+    {
+        if (mCurrentItem == null)
+            return false;
+
+        return (mCurrentItem.Name == itemName);
+    }
+
+    public InventoryItemBase GetCurrentItem()
+    {
+        return mCurrentItem;
+    }
+
+    public bool IsArmed
+    {
+        get
+        {
+            if (mCurrentItem == null)
+                return false;
+
+            return mCurrentItem.ItemType == EItemType.Weapon;
+        }
+    }
+
+
+    public void Eat(int amount)
+    {
+        Food += amount;
+        if (Food > startFood)
+        {
+            Food = startFood;
+        }
+
+        mFoodBar.SetValue(Food);
+
+    }
+
+    public void Rehab(int amount)
+    {
+        Health += amount;
+        if (Health > startHealth)
+        {
+            Health = startHealth;
+        }
+
+        mHealthBar.SetValue(Health);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!mCanTakeDamage)
+            return;
+
+        Health -= amount;
+        if (Health < 0)
+            Health = 0;
+
+        mHealthBar.SetValue(Health);
+
+        if (IsDead)
+        {
+            Die();
+        }
+
+    }
+
+
+    private void Die()
+    {
+        _animator.SetTrigger("death");
+
+        if (PlayerDied != null)
+        {
+            PlayerDied(this, EventArgs.Empty);
+        }
+    }
+
+    #endregion
+
+
+    public void Talk()
+    {
+        _animator.SetTrigger("tr_talk");
+    }
+
+
+    private bool mIsControlEnabled = true;
+
+    public void EnableControl()
+    {
+        mIsControlEnabled = true;
+    }
+
+    public void DisableControl()
+    {
+        mIsControlEnabled = false;
+    }
+
+    private void UpdateCameraPosition(float speed)
+    {
+        Vector3 newCameraPosition;
+        if (!m_UseHeadBob)
+        {
+            return;
+        }
+        if (m_CharacterController.velocity.magnitude > 0 && m_CharacterController.isGrounded)
+        {
+            m_Camera.transform.localPosition =
+                m_HeadBob.DoHeadBob(m_CharacterController.velocity.magnitude +
+                                  (speed * (m_IsWalking ? 1f : m_RunstepLenghten)));
+            newCameraPosition = m_Camera.transform.localPosition;
+            newCameraPosition.y = m_Camera.transform.localPosition.y - m_JumpBob.Offset();
+        }
+        else
+        {
+            newCameraPosition = m_Camera.transform.localPosition;
+            newCameraPosition.y = m_OriginalCameraPosition.y - m_JumpBob.Offset();
+        }
+        m_Camera.transform.localPosition = newCameraPosition;
+    }
+
+    void FixedUpdate()
+    {
+        if (!IsDead)
+        {
+            // Drop item
+            if (mCurrentItem != null && Input.GetKeyDown(KeyCode.R))
+            {
+                DropCurrentItem();
+            }
+        }
+
+        float speed;
+        GetInput(out speed);
+        // always move along the camera forward as it is the direction that it being aimed at
+        Vector3 desiredMove = transform.forward * m_Input.y + transform.right * m_Input.x;
+
+        // get a normal for the surface that is being touched to move along it
+        RaycastHit hitInfo;
+        Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
+                           m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+        desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+        m_MoveDir.x = desiredMove.x * speed;
+        m_MoveDir.z = desiredMove.z * speed;
+
+
+        if (m_CharacterController.isGrounded)
+        {
+            m_MoveDir.y = -m_StickToGroundForce;
+
+            if (m_Jump)
+            {
+                m_MoveDir.y = m_JumpSpeed;
+                m_Jump = false;
+                m_Jumping = true;
+            }
+        }
+        else
+        {
+            m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
+        }
+        m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
+
+        ProgressStepCycle(speed);
+        UpdateCameraPosition(speed);
+
+        m_MouseLook.UpdateCursorLock();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!IsDead && mIsControlEnabled)
+        {
+            // Interact with the item
+            if (mInteractItem != null && Input.GetKeyDown(KeyCode.F))
+            {
+                // Interact animation
+                mInteractItem.OnInteractAnimation(_animator);
+            }
+
+            // Execute action with item
+            if (mCurrentItem != null && Input.GetMouseButtonDown(0))
+            {
+                // Dont execute click if mouse pointer is over uGUI element
+                if (!EventSystem.current.IsPointerOverGameObject())
+                {
+                    // TODO: Logic which action to execute has to come from the particular item
+                    _animator.SetTrigger("attack_1");
+                }
+            }
+
+            RotateView();
+            // the jump state needs to read here to make sure it is not missed
+            if (!m_Jump)
+            {
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+
+            if (!m_PreviouslyGrounded && m_CharacterController.isGrounded)
+            {
+                StartCoroutine(m_JumpBob.DoBobCycle());
+                m_MoveDir.y = 0f;
+                m_Jumping = false;
+            }
+            if (!m_CharacterController.isGrounded && !m_Jumping && m_PreviouslyGrounded)
+            {
+                m_MoveDir.y = 0f;
+                _animator.SetBool("is_in_air", true);
+            }
+
+            if (m_CharacterController.velocity.sqrMagnitude > 0 && (m_Input.x != 0 || m_Input.y != 0))
+            {
+                _animator.SetBool("is_in_air", false);
+                _animator.SetBool("run", true);
+            }
+            else
+            {
+                _animator.SetBool("run", false);
+            }
+
+
+            m_PreviouslyGrounded = m_CharacterController.isGrounded;
+        }
+    }
+
+    private void ProgressStepCycle(float speed)
+    {
+        if (m_CharacterController.velocity.sqrMagnitude > 0 && (m_Input.x != 0 || m_Input.y != 0))
+        {
+            m_StepCycle += (m_CharacterController.velocity.magnitude + (speed * (m_IsWalking ? 1f : m_RunstepLenghten))) *
+                         Time.fixedDeltaTime;
+        }
+
+        if (!(m_StepCycle > m_NextStep))
+        {
+            return;
+        }
+
+        m_NextStep = m_StepCycle + m_StepInterval;
+
+    }
+
+
+    public void InteractWithItem()
+    {
+        if (mInteractItem != null)
+        {
+            mInteractItem.OnInteract();
+
+            if (mInteractItem is InventoryItemBase)
+            {
+                InventoryItemBase inventoryItem = mInteractItem as InventoryItemBase;
+                Inventory.AddItem(inventoryItem);
+                inventoryItem.OnPickup();
+
+                if (inventoryItem.UseItemAfterPickup)
+                {
+                    Inventory.UseItem(inventoryItem);
+                }
+                Hud.CloseMessagePanel();
+                mInteractItem = null;
+            }
+            //else
+            //{
+            //    if (mInteractItem.ContinueInteract())
+            //    {
+            //        Hud.OpenMessagePanel(mInteractItem);
+            //    }
+            //    else
+            //    {
+            //        Hud.CloseMessagePanel();
+            //        mInteractItem = null;
+            //    }
+            //}
+        }
+    }
+
+    private InteractableItemBase mInteractItem = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TryInteraction(other);
+    }
+
+    private void TryInteraction(Collider other)
+    {
+        InteractableItemBase item = other.GetComponent<InteractableItemBase>();
+
+        if (item != null)
+        {
+            if (item.CanInteract(other))
+            {
+                mInteractItem = item;
+
+                Hud.OpenMessagePanel(mInteractItem);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        InteractableItemBase item = other.GetComponent<InteractableItemBase>();
+        if (item != null)
+        {
+            Hud.CloseMessagePanel();
+            mInteractItem = null;
+        }
+    }
+
+    private void GetInput(out float speed)
+    {
+        // Read input
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+
+        bool waswalking = m_IsWalking;
+
+#if !MOBILE_INPUT
+        // On standalone builds, walk/run speed is modified by a key press.
+        // keep track of whether or not the character is walking or running
+        m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
+#endif
+        // set the desired speed to be walking or running
+        speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;
+        m_Input = new Vector2(horizontal, vertical);
+
+        // normalize input if it exceeds 1 in combined length:
+        if (m_Input.sqrMagnitude > 1)
+        {
+            m_Input.Normalize();
+        }
+
+        // handle speed change to give an fov kick
+        // only if the player is going to a run, is running and the fovkick is to be used
+        if (m_IsWalking != waswalking && m_UseFovKick && m_characterController.velocity.sqrMagnitude > 0)
+        {
+            StopAllCoroutines();
+            StartCoroutine(!m_IsWalking ? m_FovKick.FOVKickUp() : m_FovKick.FOVKickDown());
+        }
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        //dont move the rigidbody if the character is on top of it
+        if (m_CollisionFlags == CollisionFlags.Below)
+        {
+            return;
+        }
+
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        body.AddForceAtPosition(m_CharacterController.velocity * 0.1f, hit.point, ForceMode.Impulse);
+    }
+
+    private void RotateView()
+    {
+        m_MouseLook.LookRotation(transform, m_Camera.transform);
+    }
+}
